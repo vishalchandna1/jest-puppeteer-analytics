c5c4db07a11008f66e2cb7a74a2ab5a07cb942e2
Vishal Chandna <chandna.vishal1234@gmail.com>
2019-02-05 15:53:04 +0530

    Sinon puppeteer jest POC for analytics.

--- setup.js
+++ setup.js
@@ -9,10 +9,21 @@ const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup')
 
 module.exports = async function() {
   console.log(chalk.green('Setup Puppeteer'))
-  const browser = await puppeteer.launch({})
+  const browser = await puppeteer.launch({devtools: true});
   // This global is not available inside tests but only in global teardown
   global.__BROWSER_GLOBAL__ = browser
   // Instead, we expose the connection details via file system to be used in tests
   mkdirp.sync(DIR)
+
+  // Getting the base url of current environment.
+  const args = process.argv;
+  let BASE_URL = '';
+  for (var i = 0; i < args.length; i++) {
+    const arg = args[i];
+    if (arg.includes('-u')) {
+      BASE_URL = arg.split('-u=')[1]
+    }
+  }
+  process.baseURL = BASE_URL;
   fs.writeFileSync(path.join(DIR, 'wsEndpoint'), browser.wsEndpoint())
 }
