9fae10df14ecc2301a50790501b10a7ba3f9724d
Vishal Chandna <chandna.vishal1234@gmail.com>
2019-02-05 16:37:23 +0530

    Test changed for only to test the slick active element of the first hero container.

--- __tests__/test1.js
+++ __tests__/test1.js
@@ -1,20 +1,20 @@
-const timeout = 15000
-const chalk = require('chalk')
+const timeout = 15000;
+const chalk = require('chalk');
 // const sinon = require('sinon');
 const PubSub = require('pubsub-js');
 
 describe(
   '/ (Home Page)',
   () => {
-    let page
-    let window
-    let main
+    let page;
+    let window;
+    let main;
     beforeAll(async () => {
-      page = await global.__BROWSER__.newPage()
+      page = await global.__BROWSER__.newPage();
 
       // We can make it dyniamic by taking the BASE_URL from the terminal using npm test -- -u="http://local.newell.com/"
       await page.goto(process.baseURL);
-      await page.addScriptTag({url: 'https://cdnjs.cloudflare.com/ajax/libs/sinon.js/7.2.3/sinon.min.js'})
+      await page.addScriptTag({url: 'https://cdnjs.cloudflare.com/ajax/libs/sinon.js/7.2.3/sinon.min.js'});
       await page.exposeFunction('LogSuccess', (data) => {
         return console.log(chalk.green(data));
       });
@@ -26,13 +26,15 @@ describe(
 
 
     afterAll(async () => {
-      await page.close()
+      await page.close();
     })
 
     it('Testing analytics click event without error', async () => {
       const data = await page.evaluate(async () => {
-        const $container = jQuery('#slick-slide30');
-        const elementData = $container.find('.hero-slide').data();
+        const $containers = jQuery('.hero');
+        const $wrapper = jQuery($containers[0]).find('.slick-active');
+        const $element = $wrapper.find('.hero-slide');
+        const elementData = $element.data();
         const analyticsData = {
           event_name: 'promo_click_hero-carousel',
           event_entity: 'Promotion',
@@ -41,24 +43,24 @@ describe(
           event_label: 'promotions|' + elementData.campaignName,
           event_location: window.document.location.pathname,
           promo_click_name: 'component|' + elementData.bannerName,
-          promo_click_position: 'component|' + ($container.data().slickIndex + 1),
+          promo_click_position: 'component|' + ($wrapper.data().slickIndex + 1),
           promo_click_creative: drupalSettings.newell.page_name + ' hero carousel',
         }
         const spy = window.sinon.spy(window.utag, 'link');
         
         // Triggering the click event to which would be spied by sinon.
-        await $container.find('.hero-slide').trigger('click');
+        await $element.trigger('click');
 
         // Testing click event only done once.
-        LogCurrentStatus('Testing - Only one click on the element.')
+        LogCurrentStatus('Testing - Only one click on the element.');
         sinon.assert.calledOnce(spy);
-        LogSuccess("Success")
+        LogSuccess("Success");
 
         // Verifying each entity of the analytics payload object.
         for (let i in analyticsData) {
-          LogCurrentStatus(`Testing analyticsObject key - ${i}`)
-          sinon.assert.calledWith(spy, sinon.match({ [i]: analyticsData[i] }))
-          LogSuccess("Success")
+          LogCurrentStatus(`Testing analyticsObject key - ${i}`);
+          sinon.assert.calledWith(spy, sinon.match({ [i]: analyticsData[i] }));
+          LogSuccess("Success");
         }
 
         LogSuccess("Analytics Tests Successfully Passed for Hero");
